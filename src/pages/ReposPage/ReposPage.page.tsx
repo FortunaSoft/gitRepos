b300a8830f6237a64b1d361c7f3cf8ca6e12475d
Ilija <ilija.ika.stankovic2@gmail.com>
2023-01-05 15:56:46 +0100

    feat: ReposPage

---
+++ src/pages/ReposPage/ReposPage.page.tsx
@@ -0,0 +1,50 @@
+import React, { useRef, useState } from "react";
+import classNames from "classnames";
+
+import ReposNavigation from "../../components/ReposNavigation";
+import Button from "../../components/Button";
+import RepoList from "../../components/ReposList";
+import { SearchOptions } from "../../types/services";
+import Footer from "../../components/Footer";
+import "./ReposPage.styles.scss";
+
+type ReposPageProps = {
+  className?: string;
+};
+
+const ReposPage: React.FC<ReposPageProps> = (props) => {
+  const { className } = props;
+  const topics = ["react", "angular", "vue"];
+  const [options, setOptions] = useState<SearchOptions>({
+    order: "desc",
+    sort: "stars",
+    page: 1,
+    search: "react",
+  });
+
+  const footerRef = useRef();
+  const [selectedTopic, setSelectedTopic] = useState(0);
+  const classes = classNames("repos-page", className);
+
+  return (
+    <div className={classes}>
+      <ReposNavigation
+        onChange={(values) => setOptions(values)}
+        topics={topics}
+      />
+      <RepoList options={options} />
+      <Footer
+        pageChanged={(page) => {
+          setOptions({
+            order: options.order,
+            sort: options.sort,
+            page: page,
+            search: options.search,
+          });
+        }}
+      />
+    </div>
+  );
+};
+
+export default ReposPage;
