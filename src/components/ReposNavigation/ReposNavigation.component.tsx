4ad8b6b1fb9edd0f39b3ffb60197d15c8434eba2
Ilija <ilija.ika.stankovic2@gmail.com>
2023-01-05 15:09:42 +0100

    feat: navigation

---
+++ src/components/ReposNavigation/ReposNavigation.component.tsx
@@ -0,0 +1,63 @@
+import React, { useEffect, useState } from "react";
+import classNames from "classnames";
+
+import Button from "../../components/Button";
+import "./ReposNavigation.styles.scss";
+import { ButtonProps } from "../Button/Button.component";
+import { SearchOptions } from "../../types/services";
+
+type ReposNavigationProps = {
+  className?: string;
+  topics: string[];
+  onChange: (values: SearchOptions) => void;
+};
+
+const ReposNavigation: React.FC<ReposNavigationProps> = (props) => {
+  const { className, topics, onChange } = props;
+  const [sort, setSort] = useState<"stars" | "forks">("stars");
+  const [order, setOrder] = useState<"asc" | "desc">("desc");
+  const [seachTopic, setSearchTopic] = useState<string>("react");
+  const classes = classNames("repos-navigation", className);
+
+  useEffect(() => {
+    onChange({ order: order, sort: sort, page: 1, search: seachTopic });
+  }, [sort, order, seachTopic]);
+
+  return (
+    <div className={classes}>
+      <div className="repos-navigation__topics">
+        {topics.map((el, id) => {
+          return (
+            <Button
+              styleType="transparent-duke"
+              key={id}
+              onClick={() => {
+                setSearchTopic(el);
+              }}
+            >
+              {el}
+            </Button>
+          );
+        })}
+      </div>
+      <div className="repos-navigation__sorting">
+        <Button
+          onClick={() => {
+            setSort("stars");
+          }}
+        >
+          sort by stars
+        </Button>
+        <Button
+          onClick={() => {
+            setSort("forks");
+          }}
+        >
+          sort by forks
+        </Button>
+      </div>
+    </div>
+  );
+};
+
+export default ReposNavigation;
