c4dd5ad7c2561f267109e38899a6e1c15fb72ce3
Ilija <ilija.ika.stankovic2@gmail.com>
2023-01-05 16:52:31 +0100

    fix(App.tsx): added route for repo preview

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { Routes, Route, Router, BrowserRouter } from "react-router-dom";
 import ReposPage from "./pages/ReposPage";
+import RepoPreview from "./pages/RepoPreview";
 
 import "./App.css";
 
@@ -8,7 +9,8 @@ function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<ReposPage />}></Route>
+        <Route path="/" element={<ReposPage />} />
+        <Route path="/preview/:owner/:repo" element={<RepoPreview />} />
       </Routes>
     </BrowserRouter>
   );
