6c2e97bb1765cb6382a64f938a849102338859ea
Ilija <ilija.ika.stankovic2@gmail.com>
2023-01-05 19:17:49 +0100

    chore:cleanup

--- src/util/searchRepos.js
+++ src/util/searchRepos.js
@@ -1,9 +1,21 @@
-import { useState } from 'react';
+import { useState } from "react";
 
-export const searchRepos = async (q,sort="stars",order="desc",page=1,perPage=6) => {
-    return fetch(`https://api.github.com/search/repositories?q=${q}&sort=${sort}&order=${order}&per_page=${perPage}&page=${page}`)
-        .then(response => response.json())
-        .then((data) => { console.log(data); return data }) .catch((error) => {
-                console.error('Error:', error);
-              });
-}
+export const searchRepos = async (
+  q,
+  sort = "stars",
+  order = "desc",
+  page = 1,
+  perPage = 6
+) => {
+  return fetch(
+    `https://api.github.com/search/repositories?q=${q}&sort=${sort}&order=${order}&per_page=${perPage}&page=${page}`
+  )
+    .then((response) => response.json())
+    .then((data) => {
+      console.log(data);
+      return data;
+    })
+    .catch((error) => {
+      console.error("Error:", error);
+    });
+};
