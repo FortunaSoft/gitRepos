414831dcbfc897172dc646b73fb298f1a2f61bfc
Ilija <ilija.ika.stankovic2@gmail.com>
2023-01-05 15:14:09 +0100

    feat:footer

---
+++ src/components/Footer/Footer.component.tsx
@@ -0,0 +1,51 @@
+import classNames from "classnames";
+import React, { useEffect, useState } from "react";
+import ArrowLeft from "../../icons/ArrowLeft.icon";
+import ArrowRight from "../../icons/ArrowRight";
+import Button from "../Button";
+
+import "./Footer.styles.scss";
+
+type FooterProps = {
+  className?: string;
+  pageChanged: (page: number) => void;
+};
+
+const Footer: React.FC<FooterProps> = (props) => {
+  const { className, pageChanged } = props;
+  const classes = classNames("footer", className);
+
+  const [currentPage, setCurrentPage] = useState(1);
+
+  useEffect(() => {
+    pageChanged(currentPage);
+  }, [currentPage]);
+
+  return (
+    <div className={classes}>
+      <div className="footer__pagination">
+        <Button
+          onClick={() => {
+            if (currentPage > 1) {
+              setCurrentPage(currentPage - 1);
+            }
+          }}
+          icon={<ArrowLeft />}
+          className="footer__button"
+        />
+        <div className="footer__pagination__page">
+          <p>{currentPage}</p>
+        </div>
+        <Button
+          icon={<ArrowRight />}
+          onClick={() => {
+            setCurrentPage(currentPage + 1);
+          }}
+          className="footer__button"
+        />
+      </div>
+    </div>
+  );
+};
+
+export default Footer;
